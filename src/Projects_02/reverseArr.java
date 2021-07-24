bf302f43da75f128586b97360a87a74716f8f8a6
yeni <halukbilgin.uk@gmail.com>
2021-07-24 21:52:02 +0300

    24/07 project_02

---
+++ src/Projects_02/reverseArr.java
@@ -0,0 +1,52 @@
+package Projects_02;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class reverseArr {
+
+      /*
+        Given String array
+
+        reverse the String array
+
+        print the reversed array
+
+        NOTE: While printing the array dont use for loop
+        Use Arrays.toString(your_array_name);
+
+
+     */
+
+
+    /*
+
+          Verilen String array  String arrayi ters çevir ters arrayi yazdır
+
+       NOT:
+       Arrays.toString(your_array_name);  <--- kullanın
+
+     */
+
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String myStr = scan.nextLine();
+
+        String[] useThisArray = myStr.split(" ");
+
+        //        Kodunuzu yazmaya  bu satırdan itibaren yazınız.
+    }
+
+}
+
+
+
+
+
+
+
+
+
